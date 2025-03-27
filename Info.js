15023cb9f4432424dca2e8f09c00d3494b35707e
Barni <tothbarni.06@gmail.com>
2025-03-27 11:58:26 +0100

    JatekTer, Lampa, Info működés nélkül2

--- Info.js
+++ Info.js
@@ -6,13 +6,8 @@ export default class Info{
         this.#megjelenit(db)
     }
 
-<<<<<<< HEAD
     #megjelenit(db){
         this.szuloElem.innerHTML=`<p>Kapcsold le a lehető legtöbb lámpát!<br>(Legyen az összes zöld!)<br>A lekapcsolt lámpák száma: </p><h2>${db}</h2` 
-=======
-    #megjelenit(szuloElem){
-        this.szuloElem.innerHTML= szuloElem
->>>>>>> e0fd5cb9ba43128d1f5523648184a38723f47394
     }
     
 }
--- main.js
+++ main.js
@@ -3,8 +3,4 @@ import JatekTer from "./JatekTer.js";
 let jatekTerElem = document.getElementsByClassName("jatekter")[0];
 let infoElem = document.getElementsByClassName("info")[0];
 
-<<<<<<< HEAD
 new JatekTer(jatekTerElem, infoElem)
-=======
-new JatekTer(jatekTerElem);
->>>>>>> e0fd5cb9ba43128d1f5523648184a38723f47394
